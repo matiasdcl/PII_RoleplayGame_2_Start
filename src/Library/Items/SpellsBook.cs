f34491ec46743f7c0c894c594853411bc055b195
FacundoPiriz17 <facundo.piriz@correo.ucu.edu.uy>
2024-09-23 18:11:26 -0300

    Agregué el libro de magia

--- src/Library/Items/SpellsBook.cs
+++ src/Library/Items/SpellsBook.cs
@@ -2,18 +2,49 @@ namespace Ucu.Poo.RoleplayGame;
 
 public class SpellsBook: IItem, IAttackItem, IDefenseItem
 {
-    public IspellLibro[] Spells{ get;  set; }
+
+    private string name;
+    private bool magic;
+    private int attackValue;
+    private int defenseValue;
+    private List<IspellLibro> spells { get; }
+    public string Name
+    {
+        get { return this.name; }
+        private set { name = value; }
+    }
+
+    
+    public bool Magic
+    {
+        get { return this.magic; }
+        private set { magic = value; }
+    }
+    
+    public SpellsBook(string name)
+    {
+        Name = name;
+        magic = true;
+        spells = new List<IspellLibro>();
+        AttackValue = 0;
+        DefenseValue = 0;
+
+    }
 
     public int AttackValue
     {
         get
         {
             int value = 0;
-            foreach (IspellLibro spell in this.Spells)
+            foreach (IspellLibro spell in this.spells)
             { 
                 value += spell.AttackValue;
             }
             return value;
+        }
+        private set
+        { 
+            this.attackValue = value;
         }
     }
 
@@ -22,11 +53,15 @@ public class SpellsBook: IItem, IAttackItem, IDefenseItem
         get
         {
             int value = 0;
-            foreach (IspellLibro spell in this.Spells)
+            foreach (IspellLibro spell in this.spells)
             {
                 value += spell.DefenseValue;
             }
             return value;
+        }
+        set
+        {
+            this.defenseValue = value;
         }
     }
 }
